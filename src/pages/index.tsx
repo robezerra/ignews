8673ded1c3ad0606f29471dbdbf29b37f3ed12ce
Robert <robertbezerrads@gmail.com>
2022-04-21 21:41:38 -0300

    feat: cria header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
-import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
-	return <h1 className={styles.title}>Hello World</h1>;
+	return (
+		<>
+			<Head>
+				<title>Home | ig.news </title>
+			</Head>
+		</>
+	);
 }
