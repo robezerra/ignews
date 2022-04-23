9b80dae5f7742edaaf22861fa45e70b848d27ea8
Robert <robertbezerrads@gmail.com>
2022-04-22 22:26:20 -0300

    feat: armazena usuário no faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import Providers from "next-auth/providers"
+import { query as q } from 'faunadb'
+
+import { faunadb } from '../../../services/faunadb'
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,39 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await faunadb.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('users_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('users_by_email'),
+                q.Casefold(user.email)
+              ),
+            )
+          )
+        )
+        return true
+
+      } catch (e) {
+        console.log(e)
+        return false
+      }
+    }
+  }
 })
